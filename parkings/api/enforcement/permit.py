c0ab56673a36a157fba73265ed51b976dab56e6f
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-05-06 01:59:24 +0300

    wip: api: add activate and by-external-id etc.

--- parkings/api/enforcement/permit.py
+++ parkings/api/enforcement/permit.py
@@ -1,5 +1,8 @@
+from django.db import transaction
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import permissions, serializers, viewsets
+from rest_framework import mixins, permissions, serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ...models import Permit, PermitSeries
 
@@ -7,13 +10,41 @@ from ...models import Permit, PermitSeries
 class PermitSeriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = PermitSeries
-        fields = ['id', 'active']
+        fields = ['id', 'created_at', 'modified_at', 'active']
+        read_only_fields = fields
 
 
-class PermitSeriesViewSet(viewsets.ModelViewSet):
+class CreateAndReadOnlyModelViewSet(
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet):
+    pass
+
+
+class PermitSeriesViewSet(CreateAndReadOnlyModelViewSet):
     permission_classes = [permissions.IsAdminUser]
     queryset = PermitSeries.objects.all()
     serializer_class = PermitSeriesSerializer
+
+    @action(detail=True, methods=['post'])
+    def activate(self, request, pk=None):
+        with transaction.atomic():
+            obj_to_activate = self.get_object()
+            old_actives = self.queryset.filter(active=True)
+
+            if obj_to_activate.active and old_actives.count() == 1:
+                return Response({'status': 'No change'})
+
+            if not obj_to_activate.active:
+                obj_to_activate.active = True
+                obj_to_activate.save()
+
+            for obj in old_actives.exclude(pk=obj_to_activate.pk):
+                obj.active = False
+                obj.save()
+
+            return Response({'status': 'OK'})
 
 
 class PermitListSerializer(serializers.ListSerializer):
@@ -56,3 +87,10 @@ class PermitViewSet(viewsets.ModelViewSet):
         if isinstance(kwargs.get('data', {}), list):
             kwargs['many'] = True
         return super().get_serializer(*args, **kwargs)
+
+
+class ActivePermitByExternalIdViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAdminUser]
+    queryset = Permit.objects.active().exclude(external_id=None)
+    serializer_class = PermitSerializer
+    lookup_field = 'external_id'
